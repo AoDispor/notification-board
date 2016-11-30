2ec8f296d2eb073661bf6cb5454bdc9fc3481197
bmad4ever <bru_15scp@hotmail.com>
2016-11-30 18:09:44 +0000

    corrections on DB

--- createDB.sql
+++ createDB.sql
@@ -1,17 +1,21 @@
+drop table IF EXISTS notifications;
 CREATE table notifications(
-notificationId INTEGER PRIMARY KEY AUTOINCREMENT,
-username VARCHAR,
-senddate DATETIME
+notificationId INTEGER PRIMARY KEY AUTO_INCREMENT,
+username VARCHAR(50),
+sentdate DATETIME
 );
 
-CREATE table codigoPostal{
-codigoPostalId INTEGER PRIMARY KEY
-}
 
-CREATE table joinCodigoPostalNotifications{
+drop table IF EXISTS codigoPostal;
+CREATE table codigoPostal(
+codigoPostalId INTEGER PRIMARY KEY
+);
+
+drop table IF EXISTS joinCodigoPostalNotifications;
+CREATE table joinCodigoPostalNotifications(
 notifID INTEGER,
 codPostalId INTEGER,
-PRIMARY KEY (notifID,codPostalId),
 FOREIGN KEY (notifID) REFERENCES notifications(notificationId) ON DELETE CASCADE,
-FOREIGN KEY (codPostalId) REFERENCES codigoPostal(codigoPostalId) ON DELETE CASCADE
-}
+FOREIGN KEY (codPostalId) REFERENCES codigoPostal(codigoPostalId) ON DELETE CASCADE,
+PRIMARY KEY (notifID,codPostalId)
+);
