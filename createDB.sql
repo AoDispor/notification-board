36ee174a7c8eacb028e8e026c6b5544f2bb124b9
Bruno Madeira <bru_15scp@hotmail.com>
2016-11-30 16:13:55 +0000

    database creating sql

---
+++ createDB.sql
@@ -0,0 +1,18 @@
+CREATE table notifications(
+notificationId INTEGER PRIMARY KEY AUTOINCREMENT,
+username VARCHAR,
+senddate DATETIME
+);
+
+CREATE table codigoPostal{
+codigoPostalId INTEGER PRIMARY KEY AUTOINCREMENT,
+cp4 VARCHAR(4)
+}
+
+CREATE table joinCodigoPostalNotifications{
+notifID INTEGER,
+codPostalId INTEGER,
+PRIMARY KEY (notifID,codPostalId),
+FOREIGN KEY (notifID) REFERENCES notifications(notificationId) ON DELETE CASCADE,
+FOREIGN KEY (codPostalId) REFERENCES codigoPostal(codigoPostalId) ON DELETE CASCADE
+}
